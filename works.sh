bb0f8a75b4364b375884da2cc9e851617835b70e
Jean-Pierre Sevigny <41591249+sevignyj@users.noreply.github.com>
2023-06-11 13:58:35 -0400

    test

--- works.sh
+++ works.sh
@@ -3,6 +3,8 @@ docker buildx build . --platform linux/arm64 --tag docker.io/jpsevigny/jptest:$t
 docker buildx build . --platform linux/amd64 --tag docker.io/jpsevigny/jptest:$tag-amd64 --push
 DOCKER_CLI_EXPERIMENTAL=enabled  docker manifest create jpsevigny/jptest:$tag --amend jpsevigny/jptest:$tag-arm64 --amend jpsevigny/jptest:$tag-amd64
 
+echo "skipping rest"
+exit 0
 docker manifest push jpsevigny/jptest:$tag
 export DOCKER_CONTENT_TRUST=0
 docker pull  jpsevigny/jptest:$tag
