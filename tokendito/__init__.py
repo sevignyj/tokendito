aab9ae016113d1d82b3e9a3174d3ee9abf806bb9
Jean-Pierre Sevigny <41591249+sevignyj@users.noreply.github.com>
2023-06-15 18:12:49 -0400

    6.7

--- tokendito/__init__.py
+++ tokendito/__init__.py
@@ -8,7 +8,7 @@ import sys
 
 from platformdirs import user_config_dir
 
-__version__ = "6.6.0"
+__version__ = "6.7.0"
 __title__ = "tokendito"
 __description__ = "Get AWS STS tokens from Okta SSO"
 __long_description_content_type__ = "text/markdown"
